815ded8a8d1459f82c25a332daacfd9d52b2bacf
sajedulislamsaju <sajedulislamsaju123@gmail.com>
2023-03-10 16:53:35 +0600

    first commit

---
+++ prompt.js
@@ -0,0 +1,15 @@
+// alert ('dosto some taka de plz.')
+
+const showAlert = () => {
+    alert ("dosto some taka de na re plz.")
+}
+
+
+const confurm = () => {
+    confirm ('do you wana take me some money .')
+}
+
+
+const somePrompt = () => {
+    prompt ('ja tui dure gia more ')
+}
